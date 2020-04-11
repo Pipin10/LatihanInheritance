1aed5efae1275a6b0e54b05436cb742bc81ffefb
Pipin10 <61896418+Pipin10@users.noreply.github.com>
2020-04-11 11:16:13 +0700

    mebuat file inheritance

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace baru.LatihanInheritance
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Person person = new Person("Mike", 29);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Minnie", 31, "2345", "Math");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Pipin", 19, "2629", "Pinnopin@gmail.com");
+            student.GetNameAndAge();
+        }
+    }
+}
